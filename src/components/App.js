b0096c1bd87a23ccc553d270697da86bffd9e176
Jemila <jemila.abulhawa@gmail.com>
2019-09-12 11:40:39 +0300

    create h1 & button SCs / pass in props #4

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Heading = styled.h1`
+  font-size: 2rem;
+`
+
+const Button = styled.button`
+  font-size: 1rem;
+  color: white;
+  padding: 5px 10px;
+  margin: 5px;
+  border: none;
+  border-radius: 4px;
+  background: ${({ type }) => (type === 'cancel' ? 'tomato' : 'indigo')};
+`
 
 const App = () => {
-  return <p>Starter</p>
+  return (
+    <>
+      <Heading>Starter</Heading>
+      <Button>Save</Button>
+      <Button type="cancel">Cancel</Button>
+    </>
+  )
 }
 
 export default App
