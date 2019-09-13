8f597d21587ef885c846572959c5038dbd248fb4
Jemila <jemila.abulhawa@gmail.com>
2019-09-13 11:05:21 +0300

    add media queries in SC - https://jsramblings.com/2018/02/04/styled-components-media-queries.html

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const color = 'white'
+const sizes = {
+  mobileS: 320,
+  mobileM: 375,
+  mobileL: 425,
+  tablet: 768,
+  laptop: 1024,
+  laptopL: 1440,
+  desktop: 2560
+}
+
+const device = Object.keys(sizes).reduce((acc, cur) => {
+  acc[cur] = `(min-width: ${sizes[cur]}px)`
+  return acc
+}, {})
 
 const Button = styled.button`
   font-size: 1rem;
-  color: ${color};
+  color: white;
   padding: 5px 10px;
   margin: 5px;
   border: none;
@@ -26,6 +39,9 @@ const AppWrapper = styled.div`
   }
   ${Button} {
     margin-bottom: 2rem;
+  }
+  @media ${device.laptop} {
+    background: red;
   }
 `
 
