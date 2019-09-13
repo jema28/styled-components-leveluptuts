072788b4b4f453f011db1dd4644ae28552625eb2
Jemila <jemila.abulhawa@gmail.com>
2019-09-13 12:55:53 +0300

    add polished normalize and lighten functions #16 - examples of what you can use in polished: polished.js.org

--- src/elements/Buttons.js
+++ src/elements/Buttons.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
-import { elevation } from '../utilities'
+import { lighten } from 'polished'
+import { purple, elevation } from '../utilities'
 
 export const Button = styled.button`
   font-size: 1rem;
@@ -10,17 +11,26 @@ export const Button = styled.button`
   border-radius: 4px;
   background: indigo;
   transition: 0.3s ease box-shadow;
+
   &:hover {
-    ${elevation[2]}
+    background: ${lighten(0.1, purple)};
+    ${elevation[2]};
   }
+
   ${({ size }) => {
     if (size === 'small') {
       return `font-size: 0.5rem`
     }
   }}
+
   ${({ type }) => {
     if (type === 'cancel') {
-      return `background: tomato;`
+      return `
+      background: tomato;
+      &:hover {
+        background: ${lighten(0.1, 'tomato')};
+        ${elevation[2]};
+      `
     }
   }}
 `
